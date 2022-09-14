25aa531d7df4c536ea9bbb70a740b163edb96819
Wim Oudshoorn <woudshoo@xs4all.nl>
2022-09-14 17:32:24 +0200

    Start writing documentation

---
+++ docs/manifest.lisp
@@ -0,0 +1,7 @@
+(:docstring-markup-format :scriba
+ :systems (:cps)
+ :documents ((:title "Constraint Programming Solver"
+              :authors ("Willem Rein Oudshoorn")
+              :output-format (:type :multi-html
+                              :template :minima)
+              :sources ("manual.scr"))))
